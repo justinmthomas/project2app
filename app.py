05cf62a6b78abd8e44909ec453c68169d5c4ad30
justinmthomas <justint@manyroads.org>
2020-02-29 09:37:20 -0500

    removed s3 code from app.py

--- app.py
+++ app.py
@@ -184,30 +184,7 @@ def upload_file():
          <input type=submit value=Upload>
     </form>
     '''
-@app.route('/sign_s3/')
-def sign_s3():
-  S3_BUCKET = os.environ.get('S3_BUCKET')
 
-  file_name = request.args.get('file_name')
-  file_type = request.args.get('file_type')
-
-  s3 = boto3.client('s3')
-
-  presigned_post = s3.generate_presigned_post(
-    Bucket = S3_BUCKET,
-    Key = file_name,
-    Fields = {"acl": "public-read", "Content-Type": file_type},
-    Conditions = [
-      {"acl": "public-read"},
-      {"Content-Type": file_type}
-    ],
-    ExpiresIn = 3600
-  )
-
-  return json.dumps({
-    'data': presigned_post,
-    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
-  })
 
 
 #app.run(host='127.0.0.1', port)
