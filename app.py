5633b162db09bce3d7335d5d121359c899065d72
justinmthomas <justint@manyroads.org>
2020-03-02 20:22:49 -0500

    updated app.py to detect encoding of csv file

--- app.py
+++ app.py
@@ -139,8 +139,8 @@ def upload_file():
      # check what the character encoding might be
     print(result)
     print(result['encoding'])
-    #file_df = pd.read_csv(file, engine='python', encoding = result['encoding'])
-    #print(file_df)
+    file_df = pd.read_csv(file, engine='python', encoding=result['encoding'])
+    print(file_df)
 
 @app.route('/headers', methods=['GET'])
 def print_headers():
